a56af2bfeb945545ca915d79285f43fef8e9d394
Max Eder Jayacc De La Cruz <maxwer1@hotmail.com>
2019-04-14 17:11:49 -0500

    final

--- big-o-notation-partial-c-sharp-master/BigONotationPartial/Logarithmic.cs
+++ big-o-notation-partial-c-sharp-master/BigONotationPartial/Logarithmic.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace BigONotationPartial
 {
     class Logarithmic
-    {
+    {//
         static void Main(string[] args)
         {
             Logarithmic logarithmic = new Logarithmic();
